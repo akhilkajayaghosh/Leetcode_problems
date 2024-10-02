3f9b798e7589f4dd6c3d6e02631b650fec43a174
Akhil K A <akhilkajayaghosh@gmail.com>
2024-10-02 14:46:29 +0530

    Medium

---
+++ 1492. The kth Factor of n/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int kthFactor(int n, int k) {
+        int count=0;
+        for(int i=1;i<=n/2;i++)
+        {
+            if(n%i==0)
+                count++;
+            if(count==k)
+                return i;
+        }
+        if(k==count+1)
+            return n;
+        return -1;
+    }
+}
