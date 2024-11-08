00b92fb733e67527ffe353cb7e4a5431c3f79c14
Akhil K A <98691342+akhilkajayaghosh@users.noreply.github.com>
2024-11-08 18:03:14 +0530

    Time: 28 ms (61.18%), Space: 13.1 MB (90.79%) - LeetHub

--- 0561-array-partition/0561-array-partition.py
+++ 0561-array-partition/0561-array-partition.py
@@ -6,6 +6,6 @@ class Solution(object):
         """
         nums.sort()
         res=0
-        for i in range(0,len(nums),2):
-            res+=min(nums[i],nums[i+1])
+        res=sum(nums[i] for i in range(0,len(nums),2))
+        
         return res
