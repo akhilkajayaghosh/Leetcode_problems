bdfab348145d8f6c1f38fb079aa59f79cb8c4ae1
Akhil K A <98691342+akhilkajayaghosh@users.noreply.github.com>
2024-12-30 07:44:01 +0530

    Time: 1 ms (77.4%), Space: 40.7 MB (71.61%) - LeetHub

--- 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.java
+++ 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.java
@@ -1,11 +1,10 @@
 class Solution {
     public int differenceOfSums(int n, int m) {
-        int num1=0,num2=0;
+        int num1=0;
+        int sum=n*(n+1)/2;
         for(int i=1;i<=n;i++)
             if(i%m!=0)
-                num1+=i;
-            else
-                num2+=i;  
-        return num1-num2;      
+                num1+=i; 
+        return num1-(sum-num1);      
     }
 }
